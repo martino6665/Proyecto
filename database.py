7b5ea174581cdb53e3d4d21e172e2bf12042c164
Martin <martin.aguilar8339@alumnos.udg.mx>
2026-05-19 16:27:40 -0600

    Actualizar la URL de la base de datos para usar 'vision_education.db' en lugar de 'sql_app.db'

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 1. Ruta absoluta blindada para Render
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SQL_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'sql_app.db')}"
-
+# En tu database.py - Cambia 'sql_app.db' por 'vision_education.db'
+SQL_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'vision_education.db')}"
 # 2. Motor de base de datos
 engine = create_engine(
     SQL_DATABASE_URL, 
